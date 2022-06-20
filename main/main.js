87e0d62dc72e9214e329eb64e64922a5fff202b3
manuavalos-git <manuavalos-git@github.com>
2022-06-20 20:06:03 -0300

    agrego busqueda de pasajes y cards

--- main/main.js
+++ main/main.js
@@ -2,6 +2,42 @@ window.onload = () => {
     ajustarFecha();
 }
 
+const cargarPasajes=()=>{
+    const div=document.getElementById("divDisponibilidad");
+    div.innerHTML=`
+    <div class="col">
+        <div class="card" style="">
+        <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card" style="">
+        <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card" style="">
+        <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
+    `;
+    return false;
+}
 function ajustarFecha() {
     var today = new Date();
     var dd = today.getDate();
