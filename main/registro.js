039354ccc912825c23e0e0065864ec3b05851d14
leoyasu <leandroyasutake@gmail.com>
2022-06-21 11:59:49 -0300

    parece que anda agregar CBUCVU

--- main/registro.js
+++ main/registro.js
@@ -64,6 +64,7 @@ const validarContraseña=(contraseña)=>{
 validartarjeta=()=>{
     const formularioTarjeta= document.getElementById("contenedor-formulario-tarjeta");
     const formularioPrincipal= document.getElementById("contenedor-formulario-principal");
+    const formularioCBUCVU= document.getElementById("contenedor-formulario-CBUCVU");
 
     const numeroTarjeta= document.getElementById("ttarjeta").value;
     const formnombres= document.getElementById("tnombres").value;
@@ -77,37 +78,72 @@ validartarjeta=()=>{
 
     if( ! expresiones.tarjeta.test(numeroTarjeta)){
             alert("Número de tarjeta no válida");
-            return false;
+            return;
         }
      if( ! expresiones.nombres.test(formnombres) || formnombres!= rnombre.value + " " + rapellido.value){
             alert("La tarjeta debe tener como titular al cliente ingresado");
-            return false;
+            return;
          }           
       if( ! expresiones.codigo.test(formcodigo)){
             alert("Código de seguridad inválido");
-            return false;
+            return;
          }   
                  
     else{
         formularioTarjeta.style.display="none";
+        formularioCBUCVU.style.display="none";
         formularioPrincipal.style.display="block";
         alert("Tarjeta agregada!");
-        return true;
+    }
+}
+
+validarCBUCVU=()=>{
+    const formularioTarjeta= document.getElementById("contenedor-formulario-tarjeta");
+    const formularioPrincipal= document.getElementById("contenedor-formulario-principal");
+    const formularioCBUCVU= document.getElementById("contenedor-formulario-CBUCVU");
+
+    const numeroBancaria= document.getElementById("CBUCVU").value;
+
+    const expresiones = {codigo: /[1-9]{22}/}
+
+    if( ! expresiones.codigo.test(numeroBancaria)){
+        alert("CBU/CVU inválido");
+        return; 
+        }  
+    else{
+        formularioTarjeta.style.display="none";
+        formularioCBUCVU.style.display="none";
+        formularioPrincipal.style.display="block";
+        alert("CBU/CVU agregado!");
     }
 }
 
 formularioTarjeta=()=>{
     const formularioTarjeta= document.getElementById("contenedor-formulario-tarjeta");
+    const formularioCBUCVU= document.getElementById("contenedor-formulario-CBUCVU");
     const formularioPrincipal= document.getElementById("contenedor-formulario-principal");
 
-    formularioTarjeta.style.display="block";
     formularioPrincipal.style.display="none";
+    formularioCBUCVU.style.display="none";
+    formularioTarjeta.style.display="block";
+}
+
+formularioCBUCVU=()=>{
+    const formularioTarjeta= document.getElementById("contenedor-formulario-tarjeta");
+    const formularioCBUCVU= document.getElementById("contenedor-formulario-CBUCVU");
+    const formularioPrincipal= document.getElementById("contenedor-formulario-principal");
+    
+    formularioPrincipal.style.display="none";
+    formularioTarjeta.style.display="none";
+    formularioCBUCVU.style.display="block";
 }
 
 volveratras=(b)=>{
     const formularioTarjeta= document.getElementById("contenedor-formulario-tarjeta");
+    const formularioCBUCVU= document.getElementById("contenedor-formulario-CBUCVU");
     const formularioPrincipal= document.getElementById("contenedor-formulario-principal");
 
     formularioTarjeta.style.display="none";
+    formularioCBUCVU.style.display="none";
     formularioPrincipal.style.display="block";
 }
